e2aac2cde063d92a2cc2cb873e1dc101e444af07
coderdaudat <coderdaudat@gmail.com>
2018-11-02 15:27:08 +0700

    Create auth2

--- src/main/java/com/beemob/becastle/AuthServerApplication.java
+++ src/main/java/com/beemob/becastle/AuthServerApplication.java
@@ -2,11 +2,18 @@ package com.beemob.becastle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
+@EnableAuthorizationServer
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
 	}
+
 }
